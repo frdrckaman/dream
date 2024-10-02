9ad816317bf5c675c7ae9054a7baa8a3b58a42b6
Maquiz1 <manquiz92@gmail.com>
2024-10-02 08:46:55 +0300

    fix all

--- myjs/add/diagnosis/tb_diagnosis_made2.js
+++ myjs/add/diagnosis/tb_diagnosis_made2.js
@@ -1,19 +1,20 @@
-const tb_diagnosis_made211 = document.getElementById("tb_diagnosis_made21");
-const tb_diagnosis_made222 = document.getElementById("tb_diagnosis_made22");
+const tb_diagnosis_made2111 = document.getElementById("tb_diagnosis_made21");
+const tb_diagnosis_made2222 = document.getElementById("tb_diagnosis_made22");
 
-const laboratory_test_used2222 = document.getElementById("laboratory_test_used2");
-
+const laboratory_test_used2222 = document.getElementById(
+  "laboratory_test_used2"
+);
 
 function toggleElementVisibility() {
-  if (tb_diagnosis_made211.checked) {
+  if (tb_diagnosis_made2111.checked) {
     laboratory_test_used2222.style.display = "block";
   } else {
     laboratory_test_used2222.style.display = "none";
   }
 }
 
-tb_diagnosis_made211.addEventListener("change", toggleElementVisibility);
-tb_diagnosis_made222.addEventListener("change", toggleElementVisibility);
+tb_diagnosis_made2111.addEventListener("change", toggleElementVisibility);
+tb_diagnosis_made2222.addEventListener("change", toggleElementVisibility);
 
 // Initial check
 toggleElementVisibility();
