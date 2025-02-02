f0f862f3ecb9da44150a2bebc0c2515bee7c560b
Maquiz1 <manquiz92@gmail.com>
2025-02-03 02:18:53 +0300

    Fix_Laboratory

--- add.php
+++ add.php
@@ -280,17 +280,17 @@ if ($user->isLoggedIn()) {
                 $pid = $override->getNews('pids', 'facility_id', $user->data()->site_id, 'status', 1)[0];
                 $pid_merged = $pid['pid'] . '_' . Input::get('pid1');
 
-                // if (Input::get('conset') == 1 && (Input::get('screening_date') > $screening['conset_date'])) {
-                //     $errorMessage = 'Screaning Date Can not be greater than Conset Date';
-                // } elseif (Input::get('conset') == 2 && !empty(trim(Input::get('conset_date')))) {
-                //     $errorMessage = 'Please Remove Screening date before Submit again';
-                // } elseif ((Input::get('pid1') != Input::get('pid2')) && !empty(trim(Input::get('pid1')))) {
-                //     $errorMessage = 'PID"s are not Matching please re-check and Submit again';
-                // }
-                //  elseif ($screening['pid'] == $pid_merged) {
-                //     $errorMessage = 'PID"s Exists Please use Another';
-                // } 
-                // else {
+                if (Input::get('conset') == 1 && (Input::get('screening_date') > $screening['conset_date'])) {
+                    $errorMessage = 'Screaning Date Can not be greater than Conset Date';
+                } elseif (Input::get('conset') == 2 && !empty(trim(Input::get('conset_date')))) {
+                    $errorMessage = 'Please Remove Screening date before Submit again';
+                } elseif ((Input::get('pid1') != Input::get('pid2')) && !empty(trim(Input::get('pid1')))) {
+                    $errorMessage = 'PID"s are not Matching please re-check and Submit again';
+                }
+                 elseif ($screening['pid'] == $pid_merged) {
+                    $errorMessage = 'PID"s Exists Please use Another';
+                } 
+                else {
                     if ($screening) {
                         $user->updateRecord('screening', array(
                             'pid' => $pid_merged,
@@ -402,7 +402,7 @@ if ($user->isLoggedIn()) {
                         $successMessage = 'Screening  Successful Added';
                     }
                     Redirect::to('info.php?id=3&status=' . $_GET['status'] . '&facility_id=' . $_GET['facility_id'] . '&page=' . $_GET['page'] . '&msg=' . $successMessage);
-                // }
+                }
             } else {
                 $pageError = $validate->errors();
             }
@@ -10110,10 +10110,10 @@ if ($user->isLoggedIn()) {
     <script src="js/laboratory/formValidation.js"></script> -->
 
     <!-- Diagnosis Test format numbers Js -->
-    <script src="myjs/add/diagnosis_test/culture_performed.js"></script>
+    <script src="js/diagnosis_test/diagnosis_test_dst.js"></script>
 
     <!-- Diagnosis Js -->
-    <script src="myjs/add/diagnosis/tb_diagnosis_made.js"></script>
+    <!-- <script src="myjs/add/diagnosis/tb_diagnosis_made.js"></script> -->
 
 
     <script src="js/radio.js"></script>
