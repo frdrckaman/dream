8198d5fcf903786ebf7350e3410f6fc52a2b0fcd
Maquiz1 <manquiz92@gmail.com>
2025-02-28 06:45:42 +0300

    fix Date Validations

--- add.php
+++ add.php
@@ -8556,6 +8556,7 @@ if ($user->isLoggedIn()) {
                                                         <div class="form-group">
                                                             <label>2. Date of enrolment</label>
                                                             <input type="hidden" name="consent_date_enrollment" id="consent_date_enrollment" value="<?= $screening['consent_date'] ?>" />
+                                                            <input type="hidden" name="screening_date_enrollment" id="screening_date_enrollment" value="<?= $screening['screening_date'] ?>" />
                                                             <input class="form-control" type="date" name="enrollment_date" id="enrollment_date" value="<?= $clients['enrollment_date'] ?>" required />
                                                             <small id="enrollment_date_error" class="text-danger"></small>
                                                         </div>
