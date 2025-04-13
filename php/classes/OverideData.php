2e60f92cd968ec7930edf833020ade442723628f
Maquiz1 <manquiz92@gmail.com>
2025-04-13 23:13:13 +0300

    fox reports

--- php/classes/OverideData.php
+++ php/classes/OverideData.php
@@ -1048,4 +1048,27 @@ class OverideData
         $result = $query->fetchAll(PDO::FETCH_ASSOC);
         return $result;
     }
+
+    public function countDataWithConditions($table, $conditions)
+    {
+        $query = "SELECT COUNT(*) as count FROM $table WHERE ";
+        $params = [];
+        foreach ($conditions as $key => $value) {
+            if (strpos($key, '[>=]') !== false) {
+                $query .= str_replace('[>=]', '', $key) . " >= ? AND ";
+            } elseif (strpos($key, '[<=]') !== false) {
+                $query .= str_replace('[<=]', '', $key) . " <= ? AND ";
+            } else {
+                $query .= "$key = ? AND ";
+            }
+            $params[] = $value;
+        }
+        $query = rtrim($query, ' AND ');
+        // $result = $this->query($query, $params);
+        // return $result[0]['count'] ?? 0;
+
+        $query = $this->_pdo->query($query);
+        $result = $query->fetchAll(PDO::FETCH_ASSOC);
+        return $result;
+    }
 }
