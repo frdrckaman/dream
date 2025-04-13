899ab9c9f2a7d4e3c692c5cdaa24ad29c34b53c2
Maquiz1 <manquiz92@gmail.com>
2025-04-13 16:29:38 +0300

    fox reports

--- sidemenu.php
+++ sidemenu.php
@@ -568,6 +568,15 @@ if ($user->isLoggedIn()) {
                             </a>
                         </li>
                         <li class="nav-item">
+                            <a href="summary_reports4.php" class="nav-link">
+                                <i class="far fa-circle nav-icon"></i>
+                                <!-- <span class="badge badge-info right"> -->
+                                <!-- <?= $all; ?> -->
+                                <!-- </span> -->
+                                <p>Summary Report 4</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
                             <a href="report.php" class="nav-link">
                                 <i class="far fa-circle nav-icon"></i>
                                 <!-- <span class="badge badge-info right"> -->
