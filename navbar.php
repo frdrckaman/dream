abb6fa99772595ab4552165d2f48ca54aa1f51a1
Maquiz1 <manquiz92@gmail.com>
2025-09-11 22:14:29 +0300

    fix

--- navbar.php
+++ navbar.php
@@ -16,14 +16,14 @@
             <a href="info.php?id=3&status=1&sid=<?= $_GET['sid']; ?>&facility_id=<?= $user->data()->site_id ?>&page=<?= $_GET['page']; ?>"
                class="nav-link">Screened</a>
         </li>
-        <li class="nav-item d-none d-sm-inline-block">
+        <!-- <li class="nav-item d-none d-sm-inline-block">
             <a href="info.php?id=3&status=2&sid=<?= $_GET['sid']; ?>&facility_id=<?= $user->data()->site_id ?>&page=<?= $_GET['page']; ?>"
                class="nav-link">Eligible</a>
         </li>
         <li class="nav-item d-none d-sm-inline-block">
             <a href="info.php?id=3&status=3&sid=<?= $_GET['sid']; ?>&facility_id=<?= $user->data()->site_id ?>&page=<?= $_GET['page']; ?>"
                class="nav-link">Enrolled</a>
-        </li>
+        </li> -->
         <li class="nav-item d-none d-sm-inline-block">
             <a href="logout.php" class="nav-link">Logout</a>
         </li>
